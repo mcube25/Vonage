38cf481d70fe310d6f7f8faac63f209f5c381ee7
mcube25 <mcube3567@gmail.com>
2021-07-18 01:49:30 -0700

    higher order patterns

--- functionalProgramming .js
+++ functionalProgramming .js
@@ -84,4 +84,101 @@ describe('api', () => {
         expect(upperName('jeremy')).to.equal('JEREMY');
         expect(upperName('jet')).to.equal('JET');
     })
-})
+});
+
+//imperative/ predictable programming
+//we tell the computer how to achieve a result
+
+function doubleNumbers(numberss) {
+    const doubled = [];
+    const l = numberss.length;
+    for (let index = 0; index < l; index++) {
+        doubled.push(numberss[i] * 2);
+    }
+    return console.log(doubled);
+}
+// in the above code we are just giving a set of instruction to the function
+//we r not really declaring what we want to do
+//to write declarative code we have to declare what the desired result is
+function doubleNumbers(numberss) {
+    return numberss.map(x => x * 2);
+}
+console.log(doubleNumbers);
+//functional programming is using inbuilt mehods generally
+
+//immutable state is the principle of safety
+//create state but don't mutate it
+
+const hobbies = [
+    'programming',
+    'reading',
+    'music'
+];
+
+const firstTwo = hobbies.splice(0, 2);
+
+console.log(firstTwo);
+console.log(hobbies);
+
+//immutable js todo
+const hobbies = Object.freeze([
+    'programming',
+    'reading',
+    'music'
+]);
+
+//another example
+const createPoint = (x, y) => Object.freeze([x, y]);
+
+const movePointBy = ([x, y], dx, dx) => {
+    return Object.freeze([x + dx, y + dy]);
+};
+let point = createPoint(0, 0);
+
+point = movePointBy(point, 5, 5);
+point = movePointBy(point, -2, 3);
+console.log(point);
+
+//first class functions
+//closures/ encapsulation
+//higher order functions return a function as a value
+//closures
+const createAdder = (x) => {
+    return (y) => x + y;
+};
+
+const add3 = createAdder(3);
+
+console.log(add3);
+
+const request = (options) => {
+    return fetch(options.url, options)
+        .then(resp => resp.json())
+};
+
+const usersPromise = request({
+    url: 'userss',
+    headers: { 'x-custom': 'myKey' }
+});
+
+const tasksPromise = request({
+    url: 'tasks',
+    headers: { 'x-custom': 'myKey' }
+});
+
+// the above code is repetitive
+// we can rewrite the above code as
+const createRequester = (options) => {
+return (otherOptions) => {
+    return request(object.assign({}, options, otherOptions));
+};
+};
+
+const customRequest = createRequester({
+    headers: {'x-custom': 'mykey'}
+});
+const usersPromise = customRequest({url: '/user'});
+const tasksPromise = customRequest({url: '/tasks'});
+
+//closures are foundation for higher order patterns
+//lets take a partial application of closures
